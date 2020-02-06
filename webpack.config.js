f4350eba25e579c1d8727240a0c999e11ff0e75d
PhysRex <ThePhysRex@gmail.com>
2020-02-05 22:43:08 -0500

    add webpack configs and placeholders

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,41 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+/* 
+  TODO: Break into development and production configs
+  https://itnext.io/front-end-development-with-javascript-using-reactjs-redux-sass-and-webpack-1a2fdd46daba
+  https://developerhandbook.com/webpack/how-to-configure-scss-modules-for-webpack/
+ */
 module.exports = {
+  // entry: ['./src/index.js','./src/App.scss'],
+  // output: {
+  //     filename: 'js/main.js',
+  //     path: path.resolve(__dirname, 'dist')
+  // },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        loader: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
           }
         ]
       }
@@ -21,8 +43,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+      template: './src/index.html',
+      filename: './index.html'
     })
-  ]
+  ],
+  // resolve: { // allows imports w/out extension
+  //   extensions: ['.js', '.jsx', '.scss']
+  // }
 }
